16d86cec13850e9bc406d1748fc250418bf3ca91
Haitao Wang <45086632+apple-ouyang@users.noreply.github.com>
2021-04-15 15:59:40 +0800

    修改了一些bug

--- test_algorithm_copy_speed.sh
+++ test_algorithm_copy_speed.sh
@@ -1,12 +1,37 @@
 # script should run in root!
 if [ `whoami` != "root" ];then
         printf "please run by root!(sudo)\n"
-        exit
+        exit 1
+fi
+
+# input the test dir as argument
+if [ $# != 1 ]; then
+	printf "only input %d argument\n" $#
+	printf "usage: sudo sh ***.sh /you/test/compress/path\n" 
+	exit 1
+fi
+
+test_dir=$1
+# printf "you input path:%s\n" $test_dir
+if [ ! -d $test_dir ]; then
+	printf "path: %s not exist\n" $test_dir
+	exit 1
+fi
+
+dir_size=$(du -s $test_dir | awk '{print $1}')
+twoG=$((2*1024*1024))
+if [ $dir_size -gt $twoG ]; then
+	printf "path %s size > 2G!\n" $test_dir
+	exit 1
 fi
 
 # modprobe zram
 if [ `lsmod | grep zram | wc -l` -eq 0 ];then
 	modprobe -v zram num_devices=0	
+fi
+
+if [ $? != 0 ]; then
+	exit 1
 fi
 
 # add a zram device
@@ -19,32 +44,40 @@ printf "create zram device %s\n" $device
 
 cat /sys/block/$device/comp_algorithm
 read -p "input one of the zram algorithm: " alg
+
 # if enter empty, repeat asking
-while [ "$alg"=="" ];do
-	read -p "input the zram algorithm: " alg
+while [ -z "$alg" ];do
+	read -p "you don't input anything, input the zram algorithm: " alg
 done
 
 res_file=./${alg}.speed
 
 # find a empty zram device, set its disk size=2G, use $alg as algorithm
-sudo zramctl --find --size 2G --algorithm $alg
+printf "\ncreat zram block device:\n"
+sudo zramctl --find --size 2G --algorithm $alg 
 
 printf "all zram devices are:\n"
 zramctl
 
 # let block device zram$id be a file system
 mkdir -p $mount_dir
-mkfs.ext4 $device_dir
-mount $device_dir $mount_dir
 
-read -p "input your test file directory path (directory size < 2G): " test_file
-printf "copying $test_file to zram%i, please wait for a minute...\n" ${id}
-{ time cp -r $test_file $mount_dir/; } 2>>$res_file
-printf "#################\ndone! time use is stored in %s\n" $res_file
-sync
+mkfs.ext4 $device_dir > /dev/null
+if [ $? != 0 ]; then
+	printf "error on mkfs.ext4\n"
+	exit 1
+fi
+
+mount $device_dir $mount_dir
 
 # add zram status to result
 zramctl > $res_file
+echo "" >> $res_file
+
+printf "\ncopying $test_dir to zram%i, please wait for a minute...\n" ${id}
+(time cp -r $test_dir $mount_dir) >> $res_file 2>&1
+printf "##### done! time use is stored in %s #####\n" $res_file
+sync
 
 # umount and remove zram device
 umount $mount_dir
