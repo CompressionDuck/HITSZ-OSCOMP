5df9be0229ce94f7b945eafd5a0a0c3029e77714
apple-ouyang <1390334459@qq.com>
2021-04-26 21:07:12 +0800

    update

--- test_swap/fill_mem.c
+++ test_swap/fill_mem.c
@@ -20,8 +20,8 @@ int main(int argc, char** argv) {
         //memset(buffer, 1, multiplier * 1024*1024);
         mb++;
         printf("Allocated %d MB\n", multiplier * mb);
-        sleep(1); // time unit: 1 second
-        // usleep(0.1) sleep milisecond
+        //sleep(1); // time unit: 1 second
+        usleep(0.1); // sleep milisecond
     }      
     return 0;
 }
