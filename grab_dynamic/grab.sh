02aa4df32576d40b05e3bd9879645d02eb4a3626
apple-ouyang <1390334459@qq.com>
2021-04-26 21:06:18 +0800

    printk the anoy data

--- grab_dynamic/grab.sh
+++ grab_dynamic/grab.sh
@@ -1,10 +1,19 @@
 # /bin/sh
 
-# clear dmesg_log
-echo "" > dmesg_log
+file=log
 
-log_buf_size = $((8*1024*1024)) # 8M
+# clear file
+echo "" > $file
+
+log_buf_size=$((8*1024*1024)) # 8M
+
+# clear OS boot info
+dmesg --clear
+
+# grab zram_swap info
 while true; do
-    # show realtime, read log and clear it, set buffer size = 8M
-    dmesg --read-clear --reltime --buffer-size $log_buf_size >> dmesg_log
+    # Don't show time, read log and clear it, set buffer size = 8M
+    dmesg --read-clear --notime --buffer-size $log_buf_size >> $file
+    du -sh $file
+    sleep 0.01
 done
--- grab_dynamic/start_zram.sh
+++ grab_dynamic/start_zram.sh
@@ -4,9 +4,18 @@ if [ $(whoami) != "root" ];then
 	exit 1
 fi
 
+if [ -n "$(swapon -s | grep zram0)" ]; then
+	swapoff /dev/zram0
+fi
+
+if [ -n "$(swapon -s | grep swapfile)" ]; then
+	swapoff /swapfile
+fi
+
 printf "List all swap devices:\n\n"
-swapon -s
+
 if [ -n "$(swapon -s)" ]; then
+	swapon -s
 	printf "\n### please swapoff all above devices! ###\n"
 	printf "so we can use zram as the only swap device\n"
 	printf "example: sudo swapoff /swapfile\n"
@@ -17,13 +26,21 @@ else
 fi
 
 printf "\n### enable zram device as the only swap device ###\n\n"
-rmmod zram
+# modprobe -v zram num_devices=1
+
+if [ -n "$(lsmod | grep zram)" ]; then
+	rmmod zram;
+fi
+
 insmod ./zram.ko
-modprobe -v zram num_devices=1
-echo 1G > /sys/block/zram0/disksize
+
+zramctl -f -s 1G
 
 mkswap /dev/zram0
 swapon /dev/zram0
 
 sysctl vm.swappiness=100
+
+printf "\n### zram has started! ###\n"
+zramctl
 
